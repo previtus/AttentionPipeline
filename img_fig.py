48fe84e4c3a127965ba741053dd8e7de966e5520
previtus <previtus@gmail.com>
2017-09-29 17:33:04 -0400

    show or save mixed image

--- img_fig.py
+++ img_fig.py
@@ -3,22 +3,31 @@
 import os
 from PIL import Image
 
-#from matplotlib.pyplot import imshow
-#import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import numpy as np
 
 # paths:
 images_folder = "/home/ekmek/saliency-salgan-2017/images/"
-saliency_folder = "/home/ekmek/saliency-salgan-2017/saliency/"
+#saliency_folder = "/home/ekmek/saliency-salgan-2017/saliency/"
+
+#saliency_folder = "/home/ekmek/saliency-salgan-2017/__model size/saliency (192, 256)/"
+#saliency_folder = "/home/ekmek/saliency-salgan-2017/__model size/saliency (384, 512)/"
+saliency_folder = "/home/ekmek/saliency-salgan-2017/__model size/saliency (768, 1024)/"
+
+#save_mixes_folder = "/home/ekmek/saliency-salgan-2017/__model size/mixed (192, 256)/"
+save_mixes_folder = "/home/ekmek/saliency-salgan-2017/__model size/mixed (768, 1024)/"
 
 image_files = os.listdir(images_folder)
 saliency_files = os.listdir(saliency_folder)
 
-for i in [0]: #range(0,len(image_files)):
+for i in range(0,2):
+#for i in range(0,len(image_files)):
     img_path = images_folder+image_files[i]
     sal_path = saliency_folder+saliency_files[i]
 
     img = Image.open(img_path)
-    sal = Image.open(sal_path).convert("RGB")
+    sal = Image.open(sal_path).convert("RGB").resize(img.size, Image.ANTIALIAS)
 
     print img.mode, img.size, sal.mode, sal.size
 
@@ -27,7 +36,7 @@ for i in [0]: #range(0,len(image_files)):
 
     mix = Image.blend(img, sal, alpha=0.9)
 
+    #mix.save(save_mixes_folder+image_files[i])
     mix.show()
-    #imshow(np.asarray(mix))
 
-    print img, sal
+    #print img, sal
