41da0d0ee9a3d77de43a5d6de15bb6ac1b85a2cf
previtus <previtus@gmail.com>
2017-09-29 12:19:44 -0400

    show blended image

--- img_fig.py
+++ img_fig.py
@@ -1,6 +1,33 @@
 # show figures of images
 
-# paths:
-images_folder = "/home/ekmek/saliency-salgan-2017/images"
-saliency_folder = "/home/ekmek/saliency-salgan-2017/saliency"
+import os
+from PIL import Image
 
+#from matplotlib.pyplot import imshow
+#import numpy as np
+
+# paths:
+images_folder = "/home/ekmek/saliency-salgan-2017/images/"
+saliency_folder = "/home/ekmek/saliency-salgan-2017/saliency/"
+
+image_files = os.listdir(images_folder)
+saliency_files = os.listdir(saliency_folder)
+
+for i in [0]: #range(0,len(image_files)):
+    img_path = images_folder+image_files[i]
+    sal_path = saliency_folder+saliency_files[i]
+
+    img = Image.open(img_path)
+    sal = Image.open(sal_path).convert("RGB")
+
+    print img.mode, img.size, sal.mode, sal.size
+
+    #img.show()
+    #sal.show()
+
+    mix = Image.blend(img, sal, alpha=0.9)
+
+    mix.show()
+    #imshow(np.asarray(mix))
+
+    print img, sal
