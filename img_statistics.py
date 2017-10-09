1925b428b6256200a3ec1e77e3036d7478a0bf32
previtus <previtus@gmail.com>
2017-10-09 10:28:55 -0400

    rough image statistics over a folder

---
+++ img_statistics.py
@@ -0,0 +1,44 @@
+voc2012 = "/home/ekmek/Downloads/datasets/pascal voc 2012/VOCdevkit/VOC2012/JPEGImages/"
+cat2000 = "/home/ekmek/Downloads/datasets/CAT2000 saliency/trainSet/Stimuli/Action/"
+salicon_challenge = "/home/ekmek/Downloads/datasets/salicon-2017-challenge/images/"
+mit300 = "/home/ekmek/Downloads/datasets/MIT300/BenchmarkIMAGES/BenchmarkIMAGES/"
+
+
+folder = cat2000
+
+import os
+import numpy
+from scipy import misc
+
+from os import listdir
+from os.path import isfile, join
+image_files = [f for f in listdir(folder) if isfile(join(folder, f))]
+
+hs = []
+ws = []
+ds = []
+
+for i in range(0,100):
+#for i in range(0,len(image_files)):
+
+    img_path = folder+image_files[i]
+    #print img_path
+
+    img = misc.imread(img_path)
+    h = len(img)
+    w = len(img[0])
+    d = len(img[0][0])
+
+    #print img_path, h, w, d, img.shape
+
+    hs.append(h)
+    ws.append(w)
+    ds.append(d)
+
+hs = numpy.asarray(hs)
+ws = numpy.asarray(ws)
+ds = numpy.asarray(ds)
+
+print "heights", hs.mean(), "max", hs.max(), "min", hs.min()
+print "widths", ws.mean(), "max", ws.max(), "min", ws.min()
+print "depths", ds.mean(), "max", ds.max(), "min", ds.min()
