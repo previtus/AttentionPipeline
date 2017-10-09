7809bea4c4ba42fd5f323a0bdb4da5f8d2e79f7d
previtus <previtus@gmail.com>
2017-10-09 10:45:47 -0400

    folder paths

--- img_fig.py
+++ img_fig.py
@@ -7,22 +7,18 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 import numpy as np
 
+
+
 # paths:
-images_folder = "/home/ekmek/saliency-salgan-2017/images/"
-#saliency_folder = "/home/ekmek/saliency-salgan-2017/saliency/"
-
-#saliency_folder = "/home/ekmek/saliency-salgan-2017/__model size/saliency (192, 256)/"
-#saliency_folder = "/home/ekmek/saliency-salgan-2017/__model size/saliency (384, 512)/"
-saliency_folder = "/home/ekmek/saliency-salgan-2017/__model size/saliency (768, 1024)/"
-
-#save_mixes_folder = "/home/ekmek/saliency-salgan-2017/__model size/mixed (192, 256)/"
-save_mixes_folder = "/home/ekmek/saliency-salgan-2017/__model size/mixed (768, 1024)/"
+images_folder = "/home/ekmek/saliency_tools/_sample_inputs/images/"
+saliency_folder = "/home/ekmek/saliency_tools/_sample_inputs/heatmaps/"
+save_mixes_folder = "/home/ekmek/saliency_tools/_sample_inputs/mixes/"
 
 image_files = os.listdir(images_folder)
 saliency_files = os.listdir(saliency_folder)
 
-for i in range(0,2):
-#for i in range(0,len(image_files)):
+#for i in range(0,2):
+for i in range(0,len(image_files)):
     img_path = images_folder+image_files[i]
     sal_path = saliency_folder+saliency_files[i]
 
@@ -36,7 +32,7 @@ for i in range(0,2):
 
     mix = Image.blend(img, sal, alpha=0.9)
 
-    #mix.save(save_mixes_folder+image_files[i])
-    mix.show()
+    mix.save(save_mixes_folder+image_files[i])
+    #mix.show()
 
     #print img, sal
