ad96d9982c784cdef08be712c105914f3df222b7
previtus <previtus@gmail.com>
2017-10-11 17:32:38 -0400

    add to last commit

--- models/model_crops.py
+++ models/model_crops.py
@@ -19,36 +19,56 @@ sys.path.append("..")
 
 from data.data_handler import *
 from helpers import visualize_history
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
 from keras.applications.resnet50 import ResNet50
+from keras.applications.inception_v3 import InceptionV3
+from keras.applications.xception import Xception
+#from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.losses import mean_squared_error
+
+from timeit import default_timer as timer
 
 data_train, data_val = default_load()
 
 train = np.transpose(data_train)
 t_filenames = train[0]
-train_labels = np.array(train[1])
+t_ids = train[1]
+train_labels = np.array(train[2])
 
 val = np.transpose(data_val)
 v_filenames = val[0]
-validation_labels = np.array(val[1])
+v_ids = val[1]
+validation_labels = np.array(val[2])
 
 print "training dataset:", len(t_filenames), "image files"
 print "validation dataset:", len(v_filenames), "image files"
 
-features_need_cooking = False
-
 # small version
-#v_filenames = v_filenames[0:20]
-#validation_labels = validation_labels[0:20]
-#t_filenames = t_filenames[0:20]
-#train_labels = train_labels[0:20]
+#v_filenames = v_filenames[0:21]
+#validation_labels = validation_labels[0:21]
+#t_filenames = t_filenames[0:21]
+#train_labels = train_labels[0:21]
 
 #### BASE MODEL
 # n images * 224x224x3 ----[ ResNet50 ]- Features
 
 input_shape = None
 model = ResNet50(weights='imagenet', include_top=False, input_shape=input_shape)
-filename_features_train = "train_features_cropdata.npy"
-filename_features_test = "val_features_cropdata.npy"
+# Feature size by model:
+# ResNet50 1, 1, 2048
+# VGG16 7, 7, 512
+# VGG19 7, 7, 512
+# InceptionV3 5, 5, 2048
+# Xception 7, 7, 2048
+
+filename_features_train = "train_features_cropdata_Resnet_3clusters.npy"
+filename_features_test = "val_features_cropdata_Resnet_3clusters.npy"
+features_need_cooking = True
+
+#filename_features_train = "train_features_cropdataTEST.npy"
+#filename_features_test = "val_features_cropdataTEST.npy"
+#features_need_cooking = True
 
 if features_need_cooking:
 
@@ -98,22 +118,127 @@ from keras.layers import Input, concatenate, GlobalAveragePooling2D
 
 img_features_input = Input(shape=(1, 1, 2048))
 top = Flatten()(img_features_input)
-top = Dense(256, activation='relu')(top)
-top = Dropout(0.5)(top)
-top = Dense(256, activation='relu')(top)
-top = Dropout(0.5)(top)
+top = Dense(32, activation='relu')(top)
+top = Dropout(0.6)(top)
 output = Dense(1, activation='sigmoid')(top)
 
 model = Model(inputs=img_features_input, outputs=output)
 
 #model.summary()
 epochs = 100
-batch_size = 256
+batch_size = 255
 
-model.compile(optimizer='rmsprop', loss='mean_squared_error')
+from keras import backend as K
+import tensorflow as tf
+from tensorflow.python.framework import dtypes
+
+
+def grouped_mse(k=3):
+    def f(y_true, y_pred):
+        group_by = tf.constant(k)
+        real_size = tf.size(y_pred)
+
+        remainder = tf.truncatemod(real_size, group_by)
+        remainder = K.print_tensor(remainder, message="remainder is: ")
+
+        # ignore the rest
+        y_true = y_true[0:real_size - remainder]
+        y_pred = y_pred[0:real_size - remainder]
+
+        real_size = tf.size(y_pred) + 0*remainder
+        real_size = K.print_tensor(real_size, message="real_size is: ")
+        n = real_size / group_by
+        n = K.print_tensor(n, message="n is: ")
+
+
+        idx = tf.range(n)
+        idx = tf.reshape(idx, [-1, 1])  # Convert to a len(yp) x 1 matrix.
+        idx = tf.tile(idx, [1, group_by])  # Create multiple columns.
+        idx = tf.reshape(idx, [-1])  # Convert back to a vector.
+
+
+        y_pred_byK = tf.segment_mean(y_pred, idx) #segment_ids should be the same size as dimension 0 of input.
+        y_true_byK = tf.segment_mean(y_true, idx) #segment_ids should be the same size as dimension 0 of input
+        tmp = K.mean(K.square(y_pred_byK - y_true_byK), axis=-1)
+        return tmp
+        """
+        # whoops how can it be n/3 sized loss?
+        a = tf.size(y_pred_byK)
+        b = tf.size(y_true_byK)
+
+        a = tf.cast(a, tf.float32)
+        b = tf.cast(b, tf.float32)
+
+        a = K.print_tensor(a, message="a is: ")
+        b = K.print_tensor(b, message="b is: ")
+
+        tmp = y_pred_byK - y_true_byK
+        tmp = K.print_tensor(tmp, message="tmp1 is: ")
+        tmp = K.square(tmp)
+        tmp = K.print_tensor(tmp, message="tmp2 is: ")
+        tmp = K.mean(tmp, axis=-1)
+        tmp = K.print_tensor(tmp, message="tmp3 is: ")
+
+        tmp = tf.scalar_mul(1+0*a+0*b,tmp)
+        """
+        return tmp
+    return f
+
+k=3
+model.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=[grouped_mse(k)])
+
+# Incompatible shapes: [7] vs. [21] // n - n/3
+#model.compile(optimizer='rmsprop', loss=grouped_mse(k), metrics=['mean_squared_error'])
+
+
+start = timer()
 history = model.fit(train_data, train_labels, verbose=2,
                     epochs=epochs, batch_size=batch_size,
                     validation_data=(validation_data, validation_labels))
+end = timer()
+time = (end - start)
+
 
 history = history.history
-visualize_history(history)
+visualize_history(history,custom_title="Training, "+str(epochs)+" epochs, "+str(time)+"s",show=False,save=True,save_path='loss.png')
+
+# Evaluate the true score:
+start = timer()
+validation_pred = model.predict(validation_data)
+end = timer()
+time = (end - start)
+
+print "time of eval:", time, ", per sample:", (time/float(len(validation_labels)))
+
+
+print validation_pred.shape, validation_labels.shape
+print v_ids[0:7]
+
+# Get whichever k we chose
+#k = 0
+#while v_ids[k] == v_ids[0]:
+#    k += 1
+# spoiler, it was 3
+
+original_images = int( len(validation_labels) / k )
+
+y_true = []
+y_pred = []
+for i in range(original_images):
+    #v_ids[i*k:i*k+k]
+    ground_truth = validation_labels[i*k]
+    predicted_values = validation_pred[i*k:i*k+k]
+    prediction = np.mean(predicted_values)
+    y_true.append(ground_truth)
+    y_pred.append(prediction)
+y_true = np.array(y_true).astype(float)
+y_pred = np.array(y_pred).astype(float)
+
+print y_true.shape,y_pred.shape
+
+mse1 = np.mean(np.square(y_true - y_pred))
+
+from sklearn.metrics import mean_squared_error
+mse2 = mean_squared_error(y_true, y_pred)
+print mse1, mse2
+
