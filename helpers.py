2e2dae04d8df776f77a22dbb6265e50eafc68867
previtus <previtus@gmail.com>
2017-10-12 20:53:28 -0400

    Short summary print for keras models

--- helpers.py
+++ helpers.py
@@ -14,6 +14,17 @@ def make_dir_if_doesnt_exist(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
+def short_summary(model):
+    from keras import backend as K
+    for layer in model.layers:
+        trainable_count = int( np.sum([K.count_params(p) for p in set(layer.trainable_weights)]))
+        non_trainable_count = int( np.sum([K.count_params(p) for p in set(layer.non_trainable_weights)]))
+        if trainable_count == 0 and non_trainable_count == 0:
+            print '{:<10}[{:<10}]: {:<20} => {:<20}'.format(layer.name, layer.__class__.__name__, layer.input_shape,layer.output_shape)
+        else:
+            print '{:<10}[{:<10}]: {:<20} => {:<20}, with {} trainable + {} nontrainable'.format(layer.name, layer.__class__.__name__, layer.input_shape, layer.output_shape, trainable_count, non_trainable_count)
+
+
 def visualize_history(hi, show=True, save=False, save_path='', show_also='', custom_title=None):
     # Visualize history of Keras model run.
     '''
