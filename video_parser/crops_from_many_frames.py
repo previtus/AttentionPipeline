bdbd94a7e633f82d068f3a3a65c78ceaece14390
previtus <previtus@gmail.com>
2017-10-23 16:30:01 -0400

    When we converted Video -> Frames via FFMPEG. This should process Frames -> Crops

---
+++ video_parser/crops_from_many_frames.py
@@ -0,0 +1,7 @@
+import os
+import numpy, random
+from PIL import Image
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+folder_with_frames = "/home/ekmek/intership_project/video_parser/PL_Pizza/"
