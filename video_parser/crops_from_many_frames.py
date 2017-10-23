5d5d171bdb120db2752225aab557f25750782d5f
previtus <previtus@gmail.com>
2017-10-23 16:36:15 -0400

    Frames extraction, initial loop over folder

--- video_parser/crops_from_many_frames.py
+++ video_parser/crops_from_many_frames.py
@@ -4,4 +4,15 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-folder_with_frames = "/home/ekmek/intership_project/video_parser/PL_Pizza/"
+folder_with_frames = "/home/ekmek/intership_project/video_parser/PL_Pizza/_videoframes/"
+crops_folder = "/home/ekmek/intership_project/video_parser/PL_Pizza/"
+
+# from folder_with_frames/image000.jpg -> to crops_folder/image000/crop_0_0.jpg ... crop_n_n.jpg
+
+frame_files = sorted(os.listdir(folder_with_frames))
+
+for i in range(0,len(frame_files)):
+    frame_path = folder_with_frames+frame_files[i]
+
+    #img = Image.open(frame_path)
+    print frame_path
